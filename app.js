d3bf17cd77df956c3aa24d7d678217ee91a58b2a
Aleks_Gnatovskiy <a.gnatovskiy@do-it.co>
2018-08-09 17:15:20 +0300

    moved error handler

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const app = express();
-const middleware = require('./middleware')(app, express)
+const bodyParser = require("body-parser");
+const routes = require("./routes");
+const errorHandler = require("./error_handler")(app);
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/", routes);
+app.use(errorHandler);
 
 module.exports = app;
