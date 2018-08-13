bcc4f7a4a4862b21731e99f81943aeb7e61006e7
Aleks_Gnatovskiy <lesha.gnat@gmail.com>
2018-08-13 18:17:23 +0300

    manage to connect mongodb and create user in remote db

--- app.js
+++ app.js
@@ -9,7 +9,20 @@ app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+//MMMMMMMMM
+const mongoose = require("mongoose");
+const dev_db_url =
+  "mongodb://asterix:asterix42@ds119802.mlab.com:19802/asterix";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+const db = mongoose.connection;
 
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("DB connection is established");
+});
+//GGGGGGGGG
 app.use("/", routes);
 app.use(errorHandler);
 
--- db/index.js
+++ db/index.js
@@ -1,9 +1,10 @@
 const sqlite = require("sqlite3");
 const db = new sqlite.Database("./db/db.dev.sqlite");
 
+//MMMMMMMMM
 const mongoose = require("mongoose");
 const dev_db_url =
-  "mongodb://<asterix>:<426558>@ds119802.mlab.com:19802/asterix";
+  "mongodb://asterix:asterix42@ds119802.mlab.com:19802/asterix";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 const db = mongoose.connection;
 
@@ -13,5 +14,6 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
   console.log("DB connection is established");
 });
+//GGGGGGGGG
 
 module.exports = db;
